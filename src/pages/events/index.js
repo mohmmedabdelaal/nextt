05ac3bfbfd32a66dc1b57df33b530e6d91afeff6
mohammedabdelaal <medo.elaal@gmail.com>
2022-10-09 19:11:34 +0200

    data from backend

--- src/pages/events/index.js
+++ src/pages/events/index.js
@@ -1,12 +1,13 @@
 import { useRouter } from 'next/router';
 import EventList from '../../components/EventList';
-import { getAllEvents } from '../../Data';
+// import { getAllEvents } from '../../Data';
 import EventSearch from './EventSearch';
 
-function Events() {
+function Events({ mainEvents }) {
+  console.log(mainEvents);
   const router = useRouter();
-  const allEvents = getAllEvents();
-  console.log(allEvents);
+  // const allEvents = getAllEvents();
+  // console.log(allEvents);
   const filteredEventsHandler = (year, month) => {
     const eventsUrl = `/events/${year}/${month}`;
 
@@ -15,9 +16,38 @@ function Events() {
   return (
     <div>
       <EventSearch onSearch={filteredEventsHandler} />
-      <EventList items={allEvents} />
+      {/* <EventList items={allEvents} /> */}
     </div>
   );
+}
+
+export async function getStaticProps() {
+  const response = await fetch(
+    'https://events-d1e24-default-rtdb.firebaseio.com/events.json'
+  )
+    .then((res) => res.json())
+    .then((data) => data);
+  // const data = await response.json();
+
+  const defaultEvents = [];
+
+  for (let key of response) {
+    defaultEvents.push({
+      id: key,
+      data: data[key].date,
+      description: data[key].description,
+      image: data[key].image,
+      isFeatured: data[key].isFeatured,
+      location: data[key].location,
+      title: data[key].title,
+    });
+  }
+
+  return {
+    props: {
+      mainEvents: defaultEvents,
+    },
+  };
 }
 
 export default Events;
